27b3fd781a64ce2c437f01d84287064c15fa54bf
João Pedro Harbs <harbsprog@gmail.com>
2019-12-11 20:17:26 -0300

    Add files via upload

---
+++ APITest/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace OCRAPITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Carregar Imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Enabled = false;
+            this.txtResult.Location = new System.Drawing.Point(418, 41);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(251, 182);
+            this.txtResult.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(114, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reconhecer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Items.AddRange(new object[] {
+            "Arabic",
+            "ChineseSimplified",
+            "ChineseTraditional ",
+            "Czech",
+            "Danish",
+            "Dutch",
+            "English",
+            "Finnish",
+            "French",
+            "German",
+            "Greek ",
+            "Hungarian",
+            "Italian",
+            "Japanese",
+            "Korean  ",
+            "Norwegian",
+            "Polish",
+            "Portuguese",
+            "Russian ",
+            "Spanish",
+            "Swedish ",
+            "Turkish"});
+            this.cmbLanguage.Location = new System.Drawing.Point(288, 12);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(124, 21);
+            this.cmbLanguage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Linguagem";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblInfo.Location = new System.Drawing.Point(12, 226);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblInfo.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 182);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(572, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(38, 35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(616, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(16, 35);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(638, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(31, 35);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.Location = new System.Drawing.Point(215, 41);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(197, 182);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(418, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "AGUARDE RECONHECENDO!";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 252);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbLanguage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Reconhecimento de Placas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ APITest/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+using DIPLi;
+
+namespace OCRAPITest
+{
+    public partial class Form1 : Form
+    {
+
+        public string ImagePath { get; set; }
+        public string PdfPath { get; set; }
+        public Imagem I;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbLanguage.SelectedIndex = 6;
+        }
+
+        private string getSelectedLanguage()
+        {
+
+            string strLang = "";
+            switch (cmbLanguage.SelectedIndex)
+            {
+                case 0:
+                    strLang = "ara";
+                    break;
+
+                case 1:
+                    strLang = "chs";
+                    break;
+
+                case 2:
+                    strLang = "cht";
+                    break;
+                case 3:
+                    strLang = "cze";
+                    break;
+                case 4:
+                    strLang = "dan";
+                    break;
+                case 5:
+                    strLang = "dut";
+                    break;
+                case 6:
+                    strLang = "eng";
+                    break;
+                case 7:
+                    strLang = "fin";
+                    break;
+                case 8:
+                    strLang = "fre";
+                    break;
+                case 9:
+                    strLang = "ger";
+                    break;
+                case 10:
+                    strLang = "gre";
+                    break;
+                case 11:
+                    strLang = "hun";
+                    break;
+                case 12:
+                    strLang = "jap";
+                    break;
+                case 13:
+                    strLang = "kor";
+                    break;
+                case 14:
+                    strLang = "nor";
+                    break;
+                case 15:
+                    strLang = "pol";
+                    break;
+                case 16:
+                    strLang = "por";
+                    break;
+                case 17:
+                    strLang = "spa";
+                    break;
+                case 18:
+                    strLang = "swe";
+                    break;
+                case 19:
+                    strLang = "tur";
+                    break;
+
+            }
+            return strLang;
+
+        }
+
+       private void button1_Click(object sender, EventArgs e)
+        {
+            PdfPath = ImagePath = ""; pictureBox1.BackgroundImage = null;
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            fileDlg.Filter = "jpeg files|*.jpg;*.JPG";
+            if (fileDlg.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = new FileInfo(fileDlg.FileName);
+                if (fileInfo.Length > 5* 1024 * 1024)
+                {
+                    MessageBox.Show("Limite de tamanho de arquivo de imagem atingido (1MB free API)");
+                    return;
+                }
+                pictureBox1.Image = Image.FromFile(fileDlg.FileName);
+                ImagePath = fileDlg.FileName;
+                lblInfo.Text = "Imagem carregada: "+ fileInfo.Name;
+                lblInfo.BackColor = Color.LightGreen;
+            }
+
+            I = new Imagem(ImagePath);
+        }
+
+        public Imagem Convolucao(Imagem I, int[,] W)
+        {
+            Imagem R = new Imagem(I.Largura, I.Altura);
+
+            for (int i = 2; i < I.Altura - 2; i++)
+            {
+                for (int j = 2; j < I.Largura - 2; j++)
+                {
+                    R[i, j] = (W[0, 0] * I[i - 2, j - 2] + W[0, 1] * I[i - 1, j - 2] + W[0, 2] * I[i, j - 2] + W[1, 0] * I[i + 1, j - 2] + W[1, 1] * I[i + 2, j - 2] +
+                               W[1, 2] * I[i - 2, j - 1] + W[2, 0] * I[i - 1, j - 1] + W[2, 1] * I[i, j - 1] + W[2, 2] * I[i + 1, j - 1]) / 9;
+                }
+            }
+            return R;
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            I = I.ToGrayscale();
+
+            int[,] W = new int[,] { { 1, 1, 1 },
+                                    { 1, 1, 1 },
+                                    { 1, 1, 1 } };
+
+            int[,] W2 = new int[,] { { -1, -1, -1 },
+                                    { -1, 8, -1 },
+                                    { -1, -1, -1 } };
+
+            Imagem R = Convolucao(I, W);
+
+            pictureBox2.Image = R.ToBitmap();
+
+            //Método de Limiarização Global
+            for (int i = 0; i < R.Altura; i++)
+            {
+                for (int j = 0; j < R.Largura; j++)
+                {
+                    if (R[i, j] <= 115)
+                    {
+                        R[i, j] = 0;
+                    }
+                    else
+                    {
+                        R[i, j] = 255;
+                    }
+                }
+            }
+
+            pictureBox5.Image = R.ToBitmap();
+            R.Salvar("converter.bmp");
+
+            Imagem Y = Convolucao(R, W2);
+            pictureBox4.Image = Y.ToBitmap();
+
+            for (int i = 0; i < R.Altura; i++)
+            {
+                for (int j = 0; j < R.Largura; j++)
+                {
+                    if (R[i, j] == 255)
+                    {
+                        R[i, j] = 0;
+                    }
+                    else
+                    {
+                        R[i, j] = I[i, j];
+                    }
+                }
+            }
+            pictureBox3.Image = R.ToBitmap();
+
+
+            if (string.IsNullOrEmpty(ImagePath))
+                return;
+
+            txtResult.Text = "";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            cmbLanguage.Enabled = false;
+            label2.Visible = true;
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.Timeout = new TimeSpan(1, 1, 1);
+
+                MultipartFormDataContent form = new MultipartFormDataContent();
+                form.Add(new StringContent("2"), "OCREngine");
+                form.Add(new StringContent("f8db964da588957"), "apikey");
+                form.Add(new StringContent(getSelectedLanguage()), "language");
+
+                ImagePath = "converter.bmp";
+                if (string.IsNullOrEmpty(ImagePath) == false)
+                {
+                    byte[] imageData = File.ReadAllBytes(ImagePath);
+                    form.Add(new ByteArrayContent(imageData, 0, imageData.Length), "image", "image.jpg");
+                }
+
+                HttpResponseMessage response = await httpClient.PostAsync("https://api.ocr.space/Parse/Image", form);
+
+                string strContent = await response.Content.ReadAsStringAsync();
+
+                Rootobject ocrResult = JsonConvert.DeserializeObject<Rootobject>(strContent);
+  
+                if (ocrResult.OCRExitCode == 1)
+                  {
+                         for (int i = 0; i < ocrResult.ParsedResults.Count() ; i++)
+                         {
+                             txtResult.Text = txtResult.Text + ocrResult.ParsedResults[i].ParsedText ;
+                         }
+                     }
+                     else
+                     {
+                         MessageBox.Show("ERRO: " + strContent);
+                     }
+                    
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Algo deu errado!");
+            }
+
+            label2.ForeColor = Color.DarkGreen;
+            label2.Text = "SUCESSO";
+            txtResult.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            cmbLanguage.Enabled = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
